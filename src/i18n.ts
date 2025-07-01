55709773cce4b6b60f8e97855e8efbddfaefa18c
Josh <joshhuang9508@gmail.com>
2025-07-01 20:09:38 +0800

    temp: remove non-used vars

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import otaClient from '@crowdin/ota-client';
+// import otaClient from '@crowdin/ota-client';
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import HttpBackend from 'i18next-http-backend';
 
-const distributionHash = process.env.NEXT_PUBLIC_CROWDIN_DISTRIBUTION_HASH;
+// const distributionHash = process.env.NEXT_PUBLIC_CROWDIN_DISTRIBUTION_HASH;
 
 const localBackend = new HttpBackend(null, {
   loadPath: '/locales/{{lng}}/translation.json',
@@ -12,21 +12,21 @@ const localBackend = new HttpBackend(null, {
 
 export type LanguageKey = 'en' | 'ru' | 'ja' | 'pt-BR' | 'zh-TW' | 'zh-CN';
 
-class CrowdinBackend {
-  type = 'backend' as const;
-  client: otaClient;
+// class CrowdinBackend {
+//   type = 'backend' as const;
+//   client: otaClient;
 
-  constructor(options: { hash: string }) {
-    this.client = new otaClient(options.hash);
-  }
+//   constructor(options: { hash: string }) {
+//     this.client = new otaClient(options.hash);
+//   }
 
-  read(lang: string, ns: string, cb: any) {
-    this.client
-      .getStringsByLocale(lang)
-      .then((data) => cb(null, data)) // data => i18next JSON
-      .catch((err) => cb(err, null));
-  }
-}
+//   read(lang: string, ns: string, cb: any) {
+//     this.client
+//       .getStringsByLocale(lang)
+//       .then((data) => cb(null, data)) // data => i18next JSON
+//       .catch((err) => cb(err, null));
+//   }
+// }
 
 i18next
   .use(localBackend)
