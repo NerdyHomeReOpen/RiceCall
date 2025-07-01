b64a0a690115f5d2d1f969cb9eac455936efe58f
Josh <joshhuang9508@gmail.com>
2025-07-01 22:08:27 +0800

    feat: 更新 i18n 整合，將 Crowdin 後端實作為主要語言來源

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,20 +1,25 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 
 // Providers
-import '@/i18n';
 import SocketProvider from '@/providers/Socket';
 import ContextMenuProvider from '@/providers/ContextMenu';
 import MainTabProvider from '@/providers/MainTab';
 import ThemeProvider from '@/providers/Theme';
 import LoadingProvider from '@/providers/Loading';
+import i18n, { LanguageKey } from '@/i18n';
 
 interface ProvidersProps {
   children: React.ReactNode;
 }
 
 const Providers = ({ children }: ProvidersProps) => {
+  useEffect(() => {
+    const language = localStorage.getItem('language') as LanguageKey;
+    if (language) i18n.changeLanguage(language);
+  }, []);
+
   return (
     <ThemeProvider>
       <SocketProvider>
