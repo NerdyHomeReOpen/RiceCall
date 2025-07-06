73273a4109def5938aa38b9827c4b1e79b0fd563
Josh <joshhuang9508@gmail.com>
2025-07-06 20:16:51 +0800

    Revert "feat: 新增 i18next-chained-backend 支援，並調整 i18next 初始化配置以使用多個後端"
    This reverts commit 75c74fa63eea633374f691dc77233a2a88181115.

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,25 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import otaClient from '@crowdin/ota-client';
 import i18next from 'i18next';
-import HttpBackend from 'i18next-http-backend';
-import ChainedBackend from 'i18next-chained-backend';
 import { initReactI18next } from 'react-i18next';
 
 export type LanguageKey = 'en' | 'ru' | 'ja' | 'pt-BR' | 'zh-TW' | 'zh-CN';
 
+const hash = process.env.NEXT_PUBLIC_CROWDIN_DISTRIBUTION_HASH!;
+
 /** OTA backend */
 class CrowdinBackend {
   type = 'backend' as const;
-  client: otaClient | null = null;
-
-  init(_services: any, options: { hash: string }) {
-    if (!options.hash) throw new Error('Crowdin hash missing');
-    this.client = new otaClient(options.hash);
-  }
-
+  client = new otaClient(hash);
   read(lng: string, _ns: string, cb: any) {
-    if (!this.client) throw new Error('Crowdin client not initialized');
-
     this.client
       .getStringsByLocale(lng)
       .then((data) => cb(null, data))
@@ -28,22 +20,14 @@ class CrowdinBackend {
 }
 
 i18next
-  .use(ChainedBackend)
+  .use(new CrowdinBackend())
   .use(initReactI18next)
   .init({
-    backend: {
-      backends: [HttpBackend, CrowdinBackend],
-      backendOptions: [
-        { loadPath: '/locales/{{lng}}/{{ns}}.json' },
-        { hash: process.env.NEXT_PUBLIC_CROWDIN_DISTRIBUTION_HASH },
-      ],
-    },
-
     lng: 'zh-TW',
     fallbackLng: 'zh-TW',
     supportedLngs: ['en', 'ru', 'ja', 'pt-BR', 'zh-TW', 'zh-CN'],
 
-    ns: ['translation', 'rpc', 'message'],
+    ns: ['translation', 'rpc'],
     defaultNS: 'translation',
 
     interpolation: { escapeValue: false },
