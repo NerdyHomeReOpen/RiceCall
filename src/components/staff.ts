2ee1436b939ff08a5cee75b2b350ff35feeafc9a
Ramon <planascampoyramon@gmail.com>
2025-08-18 15:29:20 +0200

    feat: add staff.ts
    * feat: added staff.ts
    
    * feat: deleted spanish comments.
    
    * feat: refactor.

---
+++ src/components/staff.ts
@@ -0,0 +1,106 @@
+export interface Staff {
+  id: number;
+  title: string;
+  contact: string;
+  github?: string;
+  discord?: string;
+}
+
+export const staff: Staff[] = [
+  {
+    id: 0,
+    title: "developer",
+    contact: "JoshHuang9508",
+    github: "JoshHuang9508",
+    discord: "614396443016560649",
+  },
+  {
+    id: 1,
+    title: "developer",
+    contact: "yeci226",
+    github: "yeci226",
+    discord: "283946584461410305",
+  },
+  {
+    id: 2,
+    title: "developer",
+    contact: "yuusukealmal",
+    github: "yuusukealmal",
+    discord: "878830839822176287",
+  },
+  {
+    id: 3,
+    title: "developer",
+    contact: "cablate",
+    github: "cablate",
+    discord: "337525692356886538",
+  },
+  {
+    id: 4,
+    title: "developer",
+    contact: "cstrikeasia",
+    github: "cstrikeasia",
+    discord: "789742073036144640",
+  },
+  {
+    id: 5,
+    title: "developer",
+    contact: "Ramon96",
+    github: "ramonPC96",
+    discord: "493539043133620234",
+  },
+  {
+    id: 6,
+    title: "developer",
+    contact: "Hockpond",
+    github: "hockpond",
+    discord: "174274660874059776",
+  },
+  {
+    id: 7,
+    title: "machine-network",
+    contact: "Cooookie16",
+    github: "Cooookie16",
+    discord: "370537724362620930",
+  },
+  {
+    id: 8,
+    title: "machine-network",
+    contact: "yayacat",
+    github: "yayacat",
+    discord: "107918754251325440",
+  },
+  {
+    id: 9,
+    title: "machine-network",
+    contact: "codingbear",
+    github: "mcg25035",
+    discord: "492908862647697409",
+  },
+  {
+    id: 10,
+    title: "technical-support",
+    contact: "orlys",
+    github: "Orlys",
+    discord: "385825577698983937",
+  },
+  {
+    id: 11,
+    title: "technical-support",
+    contact: "5026",
+    github: "SN-Koarashi",
+    discord: "198418020329062400",
+  },
+  {
+    id: 12,
+    title: "customer-service",
+    contact: "lingyu1121",
+    discord: "371580417096155137",
+  },
+  {
+    id: 13,
+    title: "customer-service",
+    contact: "goujuanji_",
+    discord: "415152218690420736",
+  },
+];
