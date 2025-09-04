1c002ebf1529333e86523e9273a687b7ec18f7b3
Whydog <joshhuang9508@gmail.com>
2025-09-04 13:54:03 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,6 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL,
-    NEXT_PUBLIC_SFU_URL: process.env.NEXT_PUBLIC_SFU_URL,
     NEXT_PUBLIC_CROWDIN_DISTRIBUTION_HASH: process.env.NEXT_PUBLIC_CROWDIN_DISTRIBUTION_HASH,
   },
 };
