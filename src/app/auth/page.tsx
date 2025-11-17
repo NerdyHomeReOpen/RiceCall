87c1a3b9b63dfde0b857811223578d3bb965535a
Josh <joshhuang9508@gmail.com>
2025-11-18 00:20:19 +0800

    fix: update token retrieval in auto-login for improved handling
    - Modified the token retrieval logic in the auto-login function to ensure a default empty string is used if no token is found in local storage, enhancing robustness and preventing potential null reference issues.
    - Updated TypeScript configuration to include the new popupLoader module, improving the organization of included files for the Electron application.

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -73,8 +73,7 @@ const Auth: React.FC = React.memo(() => {
   useEffect(() => {
     const autoLogin = async () => {
       await new Promise((resolve) => setTimeout(resolve, 1000));
-      const token = localStorage.getItem('token');
-      if (!token) return;
+      const token = localStorage.getItem('token') || '';
       ipc.auth.autoLogin(token);
     };
     autoLogin();
