be0a6797188a4205da12aa7c4f4f526db8c18ce8
Whydog <joshhuang9508@gmail.com>
2026-03-28 17:17:10 +0800

    refactor: update PopupHeader component to use consistent styles
    - Renamed the imported CSS module from `headerStyles` to `styles` for clarity.
    - Updated class references in the PopupHeader component to align with the new styles import, enhancing maintainability and consistency across the UI.

--- src/components/common/PopupHeader.tsx
+++ src/components/common/PopupHeader.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import headerStyles from '@/styles/header.module.css';
+import styles from '@/styles/header.module.css';
 
 interface PopupHeaderProps {
   title: string;
@@ -16,15 +16,15 @@ interface PopupHeaderProps {
 
 const PopupHeader: React.FC<PopupHeaderProps> = React.memo(({ title, buttons, titleBoxIcon, isFullscreen, onMinimize, onMaximize, onRestore, onClose, className }) => {
   return (
-    <header className={`${headerStyles['header']} ${headerStyles['popup']} ${className || ''}`}>
-      <div className={headerStyles['title-wrapper']}>
-        <div className={`${headerStyles['title-box']} ${titleBoxIcon || ''}`}>
+    <header className={`${styles['header']} ${styles['popup']} ${className || ''}`}>
+      <div className={styles['title-wrapper']}>
+        <div className={`${styles['title-box']} ${titleBoxIcon || ''}`}>
           <div style={{ fontWeight: 'bold', fontSize: '13px', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>{title}</div>
         </div>
-        <div className={headerStyles['buttons']}>
-          {buttons.includes('minimize') && <div className={headerStyles['minimize']} onClick={onMinimize} />}
-          {buttons.includes('maxsize') && (isFullscreen ? <div className={headerStyles['restore']} onClick={onRestore} /> : <div className={headerStyles['maxsize']} onClick={onMaximize} />)}
-          {buttons.includes('close') && <div className={headerStyles['close']} onClick={onClose} />}
+        <div className={styles['buttons']}>
+          {buttons.includes('minimize') && <div className={styles['minimize']} onClick={onMinimize} />}
+          {buttons.includes('maxsize') && (isFullscreen ? <div className={styles['restore']} onClick={onRestore} /> : <div className={styles['maxsize']} onClick={onMaximize} />)}
+          {buttons.includes('close') && <div className={styles['close']} onClick={onClose} />}
         </div>
       </div>
     </header>
