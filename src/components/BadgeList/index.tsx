c70e58c300634a5d518effc5b9340c0f9235a8b0
Josh <joshhuang9508@gmail.com>
2026-06-28 11:47:39 +0800

    refactor: improve variable naming in BadgeList component
    - Renamed `sortedBadges` to `displayedBadges` for better clarity and consistency in the BadgeList component.
    - Updated the mapping logic to reflect the new variable name, enhancing code readability.

--- src/components/BadgeList/index.tsx
+++ src/components/BadgeList/index.tsx
@@ -15,14 +15,14 @@ interface BadgeListProps {
 }
 
 const BadgeList: React.FC<BadgeListProps> = React.memo(({ badges, position = 'left-top', direction = 'right-bottom', maxDisplay = 21, grid = false }) => {
-  const sortedBadges: Types.Badge[] = badges
+  const displayedBadges = badges
     .filter((b) => b.showTo <= 0 || b.showTo >= Date.now())
     .sort((a, b) => a.order - b.order)
     .slice(0, maxDisplay);
 
   return (
     <div className={`${styles['wrapper']} ${grid ? styles['grid'] : ''}`}>
-      {sortedBadges.map((badge) => (
+      {displayedBadges.map((badge) => (
         <BadgeItem key={badge.badgeId} badge={badge} position={position} direction={direction} />
       ))}
     </div>
